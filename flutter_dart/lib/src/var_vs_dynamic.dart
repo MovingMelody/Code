446ecbce532958a2ca6db5e6d209dd25cce8351f
Sai Sumanth <saisumanth2303@gmail.com>
2023-08-13 18:04:16 +0530

    ♻️  both dynamic & final variable

--- flutter_dart/lib/src/var_vs_dynamic.dart
+++ flutter_dart/lib/src/var_vs_dynamic.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_typing_uninitialized_variables, inference_failure_on_uninitialized_variable, prefer_const_declarations
+
 /*
 * Created on 13 August 2023
 * 
@@ -5,7 +7,7 @@
 */
 
 ///
-/// Var vs Dynamic in Dart
+/// Var 🆚 Dynamic in Dart
 ///
 void main(List<String> args) {
   ///
@@ -15,7 +17,9 @@ void main(List<String> args) {
   /// the Dart compiler infers the type of that variable based on the assigned value.
   /// Once the type is inferred, the variable's type becomes fixed and cannot be changed.
   ///
-  var score = 99;
+  /// 💡 final keyword can't be used with var 💡
+  ///
+  var score = 99; // type of score is infered as int. since we are assinging the value during declaration
   score = 98;
   score = 67;
   score = 100;
@@ -24,18 +28,49 @@ void main(List<String> args) {
   // score = 'hundred'; not allowed. Error
   print('var variable score value : $score'); // 100
 
+  /// declaring a variable of type var (observe carefully no value assigned here)
+  var unAssignedVar;
+  print('unAssignedVar runtimeType : ${unAssignedVar.runtimeType}'); // Null (with capital N - Null)
+  print('unAssignedVar value : $unAssignedVar'); // null (with small n - null)
+
+  unAssignedVar = 34; // initializing to int
+
+  /// reassigning bool value (🚨 reassining of different type is possible here 🥲 because value isn't assigned to unAssignedVar at the time of declaration)
+  unAssignedVar = bool; 
+  unAssignedVar = 'can be string also'; // reassinging string value
+
+
+
+
   ///
-  /// Type inference concept isn't applicable for variables declared with dynamic keyword.
+  /// Type inference concept isn't applicable for variables declared with dynamic 🤸‍♂️ keyword.
   /// Type can be changed at runtime.
   ///
   /// So the usage of dynamic should be minimized unless you're dealing with situations where
   /// dynamic typing is absolutely necessary.
   ///
+  /// 💡 final keyword CAN be used with dynamic 💡
+  ///
   dynamic userInput = 43;
   print(userInput.runtimeType); // int
-
-  userInput = 'forty three'; // Type changed to String
+  userInput = 'forty three 😁'; // Type changed to String
   print(userInput.runtimeType); // String
+  print('User entered : $userInput'); // forty three 😁
 
-  print('User entered : $userInput');
+  userInput = null;
+  print(userInput.runtimeType); // Null
+  print('User entered : $userInput'); // null
+
+  /// ␀
+  dynamic someXname;
+  print('someXname value : $someXname'); // null
+
+
+  ///
+  /// dynamic & final combination
+  /// 
+  final dynamic finalUserInput = 'final & dynamic';
+  // finalUserInput = 'need to mark finalUserInput as non-final to assign this value'; // Error
+  print('finalUserInput value : $finalUserInput'); // final & dynamic
+  print('finalUserInput run time type : ${finalUserInput.runtimeType}'); // String
 }
